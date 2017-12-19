799bf2e1ad3b4e24f24ec17d0590f3b64a96eb4f
Leonardo Cristofari <leonardo.cristofari@zenvia.com>
2017-12-19 18:01:11 -0200

    Added Second Activity

--- app/src/main/java/leocr/easyclockin/MainActivity.kt
+++ app/src/main/java/leocr/easyclockin/MainActivity.kt
@@ -1,10 +1,12 @@
 package leocr.easyclockin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         myToast.show()
     }
 
-    fun countMe (view: View) {
+    fun countMe(view: View) {
         // Get the text view
         val showCountTextView = findViewById<TextView>(R.id.textView) as TextView
 
@@ -31,5 +33,22 @@ class MainActivity : AppCompatActivity() {
 
         // Display the new value in the text view.
         showCountTextView.text = count.toString();
+    }
+
+    fun randomMe (view: View) {
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Get the current value of the text view.
+        val countString = textView.text.toString()
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+
+        // Start the new activity.
+        startActivity(randomIntent)
     }
 }
