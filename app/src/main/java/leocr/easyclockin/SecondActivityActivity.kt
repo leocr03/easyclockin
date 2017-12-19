a26ba6ee1a88f0511f48a6f8c6ac8660af6e08d4
Leonardo Cristofari <leocr03@gmail.com>
2017-12-18 22:08:02 -0200

    Adding second activity

---
+++ app/src/main/java/leocr/easyclockin/SecondActivityActivity.kt
@@ -0,0 +1,12 @@
+package leocr.easyclockin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_activity)
+    }
+}
