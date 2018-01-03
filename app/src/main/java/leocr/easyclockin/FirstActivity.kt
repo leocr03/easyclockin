4b8287e1bfe572081e6e5c7d25d316f487d2b5e8
Leonardo Cristofari <leonardo.cristofari@zenvia.com>
2018-01-03 11:29:12 -0200

    Initial commit

--- app/src/main/java/leocr/easyclockin/FirstActivity.kt
+++ app/src/main/java/leocr/easyclockin/FirstActivity.kt
@@ -1,12 +1,12 @@
 package leocr.easyclockin
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
